9b48d5b241d82e32100c642adf3c4f5153c29c6c
taiseen <taiseen.cse@gmail.com>
2025-05-20 02:22:49 +0600

    feat: views ejs template

--- contact-app/index.js
+++ contact-app/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 const app = express();
 
 
-
+// Middlewares...
 app.use(express.urlencoded({ extended: false })); // for process html form data
 app.use(express.static('public')); // static files
 app.use(express.json()); // for process json data form binary stream
@@ -11,17 +11,15 @@ app.set('view engine', 'ejs'); // template engine
 
 
 // Routes
-app.get('/', (req, res) => {
-    res.send("Contact App");
-})
+app.get('/', (req, res) => { res.render('home') })
 
-app.get('/show-contact', (req, res) => { })
+app.get('/show-contact', (req, res) => { res.render('show-contact') })
 
-app.get('/add-contact', (req, res) => { })
+app.get('/add-contact', (req, res) => { res.render('add-contact') })
 
 app.post('/add-contact', (req, res) => { })
 
-app.get('/update-contact', (req, res) => { })
+app.get('/update-contact', (req, res) => { res.render('update-contact') })
 
 app.post('/update-contact', (req, res) => { })
 
@@ -33,4 +31,5 @@ app.get('/delete-contact', (req, res) => { })
 
 const PORT = process.env.PORT || 3000;
 const serverListener = () => console.log(`🚀 Server is running on http://localhost:${PORT}`);
+
 app.listen(3000, serverListener);
