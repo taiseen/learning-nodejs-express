22247d7600bd9773ca7427da3c8e964eaeaedfeb
taiseen <taiseen.cse@gmail.com>
2025-06-25 20:00:50 +0600

    feat: news-cms-project init ✨

---
+++ z-news-cms-project/src/app.js
@@ -0,0 +1,26 @@
+import expressLayouts from 'express-ejs-layouts';
+import express from 'express';
+import path from 'path';
+import url from 'url';
+
+
+const app = express();
+
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+app.use(express.json());
+app.use(expressLayouts);
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('layouts', 'layouts');
+app.set('view engine', 'ejs');
+
+
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running :- http://localhost:${PORT}`));
